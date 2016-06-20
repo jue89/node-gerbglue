d4851a1752b5f1d21d8a94fff381b485ff62f386
Jue <me@jue.yt>
2016-06-20 23:06:15 +0200

    First unit test of Point class
    ... and, of course, first bug fixes ...

---
+++ test/point.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const assert = require( 'assert' );
+
+describe( "Class Point", () => {
+
+	const Format = require( '../lib/format.js' );
+	const Point = require( '../lib/point.js' );
+
+	it( "should complain about missing value", ( done ) => {
+		try {
+
+			let point = new Point( );
+
+		} catch( e ) { console.log(e); done(); }
+	} );
+
+	it( "should complain about missing srcFormat", ( done ) => {
+		try {
+
+			let point = new Point( {} );
+
+		} catch( e ) { console.log(e); done(); }
+	} );
+
+	it( "should complain about missing dstFormat", ( done ) => {
+		try {
+
+			let point = new Point( {}, new Format() );
+
+		} catch( e ) { console.log(e); done(); }
+	} );
+
+	function testConvert( iFormat, oFormat, input, output ) {
+		iFormat = new Format( iFormat );
+		oFormat = new Format( oFormat );
+		it( `should convert ${iFormat} to ${oFormat}`, ( done ) => {
+			let point = new Point( input, iFormat, oFormat );
+			assert.deepStrictEqual( point.get(), output );
+			done();
+		} );
+	}
+
+	testConvert(
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 6, zeroSupression: 'leading' },
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 4, zeroSupression: 'leading' },
+		{ x: '123456789', y:'-123456789' },
+		{ x: '1234568',   y:'-1234568' }
+	);
+
+	testConvert(
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 4, zeroSupression: 'leading' },
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 6, zeroSupression: 'leading' },
+		{ x: '1234567',   y:'-1234567' },
+		{ x: '123456700', y:'-123456700' }
+	);
+
+	testConvert(
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 6, zeroSupression: 'leading' },
+		{ type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 3, precisionPost: 3, zeroSupression: 'leading' },
+		{ x: '25400000', y: '-25400000' },
+		{ x: '1000',     y:'-1000' }
+	);
+
+	testConvert(
+		{ type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 3, precisionPost: 3, zeroSupression: 'leading' },
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 6, zeroSupression: 'leading' },
+		{ x: '1000',     y:'-1000' },
+		{ x: '25400000', y: '-25400000' }
+	);
+
+	testConvert(
+		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 3, precisionPost: 3, zeroSupression: 'leading' },
+		{ type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 2, precisionPost: 4, zeroSupression: 'leading' },
+		{ x: '95000', y: '-100000' },
+		{ x: '37402', y:'-39370' }
+	);
+
+	testConvert(
+		{ type: 'string', unit: 'metric', point: 'floating', precisionPost: 3, zeroSupression: 'leading' },
+		{ type: 'string', unit: 'imperial', point: 'floating', precisionPost: 3, zeroSupression: 'leading' },
+		{ d: '0.300' },
+		{ d: '0.012' }
+	);
+
+	it( 'should move a point', ( done ) => {
+
+		let point = new Point(
+			{ x: '1000', y: '-1000' },
+			new Format( { type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 3, precisionPost: 3, zeroSupression: 'leading' } ),
+			new Format( { type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 6, zeroSupression: 'leading' } )
+		);
+
+		point.move(
+			{ x: -0.9, d: 1 },
+			new Format( { type: 'number', unit: 'imperial', point: 'floating', precisionPost: 10 } )
+		);
+
+		assert.deepStrictEqual( point.get(), { x: '2540000', y: '-25400000' } );
+		done();
+
+	} );
+
+} );
