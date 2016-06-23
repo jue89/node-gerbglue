21e18c76b460c858d1cf30b59dbfac6bc630e73a
Jue <me@jue.yt>
2016-06-23 13:04:46 +0200

    Bug fix: Negative coordinates at Gerber operation
    Negative coordinates were ignored.

--- test/30-gerber-operation.js
+++ test/30-gerber-operation.js
@@ -2,32 +2,6 @@
 
 const assert = require( 'assert' );
 
-const inData = `X0Y50000D02*
-Y100000D01*
-X100000D01*
-Y0D01*
-X0D01*
-Y50000D01*
-D02*
-X-50000Y10000D01*
-X-90000Y50000D01*
-X-50000Y90000D01*
-X0Y50000D01*
-X50000Y60000I30000J0D01*`;
-
-const outData = `X0Y197D02*
-Y394D01*
-X394D01*
-Y0D01*
-X0D01*
-Y197D01*
-D02*
-Y39D01*
-Y197D01*
-Y354D01*
-X0Y197D01*
-X197Y236I118J0D01*`;
-
 describe( "Class GerberOperation", () => {
 
 	const Format = require( '../lib/format.js' );
@@ -39,9 +13,6 @@ describe( "Class GerberOperation", () => {
 		oFormat = new Format( oFormat );
 
 		let go = new GerberOperationReader( iFormat, oFormat );
-
-		input = input.split( '\n' );
-		output = output.split( '\n' );
 
 		it( `should convert ${iFormat} to ${oFormat}`, ( done ) => {
 
@@ -58,8 +29,34 @@ describe( "Class GerberOperation", () => {
 	testConvert(
 		{ type: 'string', unit: 'metric', point: 'fixed', precisionPre: 2, precisionPost: 4, zeroSupression: 'leading' },
 		{ type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 2, precisionPost: 3, zeroSupression: 'leading' },
-		inData,
-		outData
+		[
+			'X0Y50000D02*',
+			'Y100000D01*',
+			'X100000D01*',
+			'Y0D01*',
+			'X0D01*',
+			'Y50000D01*',
+			'D02*',
+			'X-50000Y10000D01*',
+			'X-90000Y50000D01*',
+			'X-50000Y90000D01*',
+			'X0Y50000D01*',
+			'X50000Y60000I30000J0D01*'
+		],
+		[
+			'X0Y197D02*',
+			'Y394D01*',
+			'X394D01*',
+			'Y0D01*',
+			'X0D01*',
+			'Y197D01*',
+			'D02*',
+			'X-197Y39D01*',
+			'X-354Y197D01*',
+			'X-197Y354D01*',
+			'X0Y197D01*',
+			'X197Y236I118J0D01*'
+		]
 	);
 
 } );
