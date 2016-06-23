cb1017db5a3df11ba6633d6504373b091b566770
Jue <me@jue.yt>
2016-06-23 12:40:30 +0200

    Gerber class: First unit tests

---
+++ test/40-gerber.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const assert = require( 'assert' );
+const fs = require( 'fs' );
+
+describe( "Class Gerber", () => {
+
+	const Format = require( '../lib/format.js' );
+	const ApertureStore = require( '../lib/aperture-store.js' );
+	const GerberReader = require( '../lib/gerber.js' );
+
+	const oFormat = new Format( { type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 4, precisionPost: 6 } );
+
+	it( "should read Atium Gerber files", ( done ) => {
+
+		let gf = new GerberReader(
+			'altium',
+			fs.readFileSync( './test/data/40-gerber-altium.gbr' ),
+			new ApertureStore( oFormat ),
+			oFormat
+		);
+
+		done();
+
+	} );
+
+	it( "should read KiCad Gerber files", ( done ) => {
+
+		let gf = new GerberReader(
+			'altium',
+			fs.readFileSync( './test/data/40-gerber-kicad.gbr' ),
+			new ApertureStore( oFormat ),
+			oFormat
+		);
+
+		done();
+
+	} );
+
+} );
