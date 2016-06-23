e7cacd83faec4a9ee6b3806eff15a49a2fc1dac0
Jue <me@jue.yt>
2016-06-23 13:27:55 +0200

    Output of Gerber files

--- test/40-gerber.js
+++ test/40-gerber.js
@@ -9,16 +9,18 @@ describe( "Class Gerber", () => {
 	const ApertureStore = require( '../lib/aperture-store.js' );
 	const GerberReader = require( '../lib/gerber.js' );
 
-	const oFormat = new Format( { type: 'string', unit: 'imperial', point: 'fixed', precisionPre: 4, precisionPost: 6 } );
+	const oFormat = new Format( { type: 'string', unit: 'metric', point: 'fixed', precisionPre: 4, precisionPost: 6 } );
 
 	it( "should read Atium Gerber files", ( done ) => {
 
 		let gf = new GerberReader(
 			'altium',
-			fs.readFileSync( './test/data/40-gerber-altium.gbr' ),
+			fs.readFileSync( './test/data/40-gerber-altium-in.gbr' ),
 			new ApertureStore( oFormat ),
 			oFormat
 		);
+
+		assert.strictEqual( gf.toString(), fs.readFileSync( './test/data/40-gerber-altium-out.gbr' ).toString() );
 
 		done();
 
@@ -26,12 +28,16 @@ describe( "Class Gerber", () => {
 
 	it( "should read KiCad Gerber files", ( done ) => {
 
+		let as = new ApertureStore( oFormat );
+
 		let gf = new GerberReader(
 			'altium',
-			fs.readFileSync( './test/data/40-gerber-kicad.gbr' ),
-			new ApertureStore( oFormat ),
+			fs.readFileSync( './test/data/40-gerber-kicad-in.gbr' ),
+			as,
 			oFormat
 		);
+console.log(as.toString());
+		assert.strictEqual( gf.toString(), fs.readFileSync( './test/data/40-gerber-kicad-out.gbr' ).toString() );
 
 		done();
 
