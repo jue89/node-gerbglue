a29a01fd0a7fb31025f014fbd5397571897da732
Jue <me@jue.yt>
2016-06-24 21:16:11 +0200

    Refactored slotted holes
    They have been combined with simple holes into one module. This also solves the problem addressed in commit 2426f7c.

--- test/50-excellon-hole.js
+++ test/50-excellon-hole.js
@@ -10,34 +10,42 @@ describe( "Class ExcellonHole", () => {
 	const iFormat = new Format( { type: 'string', unit: 'metric', point: 'fixed', precisionPre: 3, precisionPost: 3, zeroSupression: 'leading' } );
 	const oFormat = new Format( { type: 'string', unit: 'metric', point: 'fixed', precisionPre: 3, precisionPost: 4, zeroSupression: 'leading' } );
 
-	const input = [
+	const inputSimple = [
 		'X95000Y-100000',
 		'X-95000Y100000',
 		'X-95000',
 		'Y100000'
 	];
 
-	const output = [
+	const outputSimple = [
 		'X950000Y-1000000',
 		'X-950000Y1000000',
 		'X-950000',
 		'Y1000000'
 	];
 
-	it( `should convert ${iFormat} to ${oFormat}`, ( done ) => {
+	const inputSlotted = [
+		'X1000Y-1000G85X-1000Y1000'
+	];
+
+	const outputSlotted = [
+		'X10000Y-10000G85X-10000Y10000'
+	];
+
+	it( `should convert simple hole from ${iFormat} to ${oFormat}`, ( done ) => {
 
 		let go = new ExcellonHoleReader( iFormat, oFormat );
 
-		for( let i in input ) {
-			let op = go.fromLine( input[i] );
-			assert.strictEqual( op.toString(), output[i] );
+		for( let i in inputSimple ) {
+			let op = go.fromLine( inputSimple[i] );
+			assert.strictEqual( op.toString(), outputSimple[i] );
 		}
 
 		done();
 
 	} );
 
-	it( `should bypass operation if no boundaries are crossed`, ( done ) => {
+	it( `should bypass simple hole if no boundaries are crossed`, ( done ) => {
 
 		let go = new ExcellonHoleReader(
 			iFormat,
@@ -45,15 +53,15 @@ describe( "Class ExcellonHole", () => {
 			[ 'metric', 100, 95, -100, -95 ]
 		);
 
-		for( let i in input ) {
-			let op = go.fromLine( input[i] );
+		for( let i in inputSimple ) {
+			let op = go.fromLine( inputSimple[i] );
 		}
 
 		done();
 
 	} );
 
-	it( `should reject operation upon boundary crossing (yMax)`, ( done ) => {
+	it( `should reject simple hole upon boundary crossing (yMax)`, ( done ) => {
 
 		let go = new ExcellonHoleReader(
 			iFormat,
@@ -62,14 +70,14 @@ describe( "Class ExcellonHole", () => {
 		);
 
 		try {
-			for( let i in input ) {
-				let op = go.fromLine( input[i] );
+			for( let i in inputSimple ) {
+				let op = go.fromLine( inputSimple[i] );
 			}
 		} catch( e ) { /*console.log( e );*/ done(); }
 
 	} );
 
-	it( `should reject operation upon boundary crossing (xMax)`, ( done ) => {
+	it( `should reject simple hole upon boundary crossing (xMax)`, ( done ) => {
 
 		let go = new ExcellonHoleReader(
 			iFormat,
@@ -78,14 +86,14 @@ describe( "Class ExcellonHole", () => {
 		);
 
 		try {
-			for( let i in input ) {
-				let op = go.fromLine( input[i] );
+			for( let i in inputSimple ) {
+				let op = go.fromLine( inputSimple[i] );
 			}
 		} catch( e ) { /*console.log( e );*/ done(); }
 
 	} );
 
-	it( `should reject operation upon boundary crossing (yMin)`, ( done ) => {
+	it( `should reject simple hole upon boundary crossing (yMin)`, ( done ) => {
 
 		let go = new ExcellonHoleReader(
 			iFormat,
@@ -94,14 +102,14 @@ describe( "Class ExcellonHole", () => {
 		);
 
 		try {
-			for( let i in input ) {
-				let op = go.fromLine( input[i] );
+			for( let i in inputSimple ) {
+				let op = go.fromLine( inputSimple[i] );
 			}
 		} catch( e ) { /*console.log( e );*/ done(); }
 
 	} );
 
-	it( `should reject operation upon boundary crossing (xMin)`, ( done ) => {
+	it( `should reject simple hole upon boundary crossing (xMin)`, ( done ) => {
 
 		let go = new ExcellonHoleReader(
 			iFormat,
@@ -110,8 +118,101 @@ describe( "Class ExcellonHole", () => {
 		);
 
 		try {
-			for( let i in input ) {
-				let op = go.fromLine( input[i] );
+			for( let i in inputSimple ) {
+				let op = go.fromLine( inputSimple[i] );
+			}
+		} catch( e ) { /*console.log( e );*/ done(); }
+
+	} );
+
+	it( `should convert slotted hole from ${iFormat} to ${oFormat}`, ( done ) => {
+
+		let go = new ExcellonHoleReader( iFormat, oFormat );
+
+		for( let i in inputSlotted ) {
+			let op = go.fromLine( inputSlotted[i] );
+			assert.strictEqual( op.toString(), outputSlotted[i] );
+		}
+
+		done();
+
+	} );
+
+	it( `should bypass slotted hole if no boundaries are crossed`, ( done ) => {
+
+		let go = new ExcellonHoleReader(
+			iFormat,
+			oFormat,
+			[ 'metric', 1, 1, -1, -1 ]
+		);
+
+		for( let i in inputSlotted ) {
+			let op = go.fromLine( inputSlotted[i] );
+		}
+
+		done();
+
+	} );
+
+	it( `should reject slotted hole upon boundary crossing (yMax)`, ( done ) => {
+
+		let go = new ExcellonHoleReader(
+			iFormat,
+			oFormat,
+			[ 'metric', 0.9, 1, -1, -1 ]
+		);
+
+		try {
+			for( let i in inputSlotted ) {
+				let op = go.fromLine( inputSlotted[i] );
+			}
+		} catch( e ) { /*console.log( e );*/ done(); }
+
+	} );
+
+	it( `should reject slotted hole upon boundary crossing (xMax)`, ( done ) => {
+
+		let go = new ExcellonHoleReader(
+			iFormat,
+			oFormat,
+			[ 'metric', 1, 0.9, -1, -1 ]
+		);
+
+		try {
+			for( let i in inputSlotted ) {
+				let op = go.fromLine( inputSlotted[i] );
+			}
+		} catch( e ) { /*console.log( e );*/ done(); }
+
+	} );
+
+	it( `should reject slotted hole upon boundary crossing (yMin)`, ( done ) => {
+
+		let go = new ExcellonHoleReader(
+			iFormat,
+			oFormat,
+			[ 'metric', 1, 1, -0.9, -1 ]
+		);
+
+		try {
+			for( let i in inputSlotted ) {
+				let op = go.fromLine( inputSlotted[i] );
+			}
+		} catch( e ) { /*console.log( e );*/ done(); }
+
+	} );
+
+	it( `should reject slotted hole upon boundary crossing (xMin)`, ( done ) => {
+
+		let go = new ExcellonHoleReader(
+			iFormat,
+			oFormat,
+			[ 'metric', 1, 1, -1, -0.9 ]
+		);
+
+		try {
+			for( let i in inputSlotted ) {
+				let op = go.fromLine( inputSlotted[i] );
 			}
 		} catch( e ) { /*console.log( e );*/ done(); }
 
